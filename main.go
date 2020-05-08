5fd44845f70b23aa7308fa96ab6df5aac6c3168d
j584lee98 <57649385+j584lee98@users.noreply.github.com>
2020-05-08 18:32:37 -0400

    Add get, update and delete routes

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -26,9 +27,44 @@ var items []Item = []Item{}
 func main() {
 	router := mux.NewRouter()
 
-	router.HandleFunc("/add", addItem).Methods("POST")
+	router.HandleFunc("/posts/{id}", getItem).Methods("GET")
+
+	router.HandleFunc("/posts", getItems).Methods("GET")
+
+	router.HandleFunc("/posts", addItem).Methods("POST")
+
+	router.HandleFunc("/posts/{id}", updateItem).Methods("PUT")
+
+	router.HandleFunc("/posts/{id}", deleteItem).Methods("DELETE")
 
 	http.ListenAndServe(":5000", router)
+}
+
+func getItem(writer http.ResponseWriter, req *http.Request) {
+	var idParam string = mux.Vars(req)["id"]
+	id, err := strconv.Atoi(idParam)
+
+	if err != nil {
+		writer.WriteHeader(400)
+		writer.Write([]byte("ID couldn't be converted into an integer"))
+		return
+	}
+
+	if id >= len(items) {
+		writer.WriteHeader(404)
+		writer.Write([]byte("No item found with specified ID"))
+		return
+	}
+
+	item := items[id]
+
+	writer.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(writer).Encode(item)
+}
+
+func getItems(writer http.ResponseWriter, req *http.Request) {
+	writer.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(writer).Encode(items)
 }
 
 func addItem(writer http.ResponseWriter, req *http.Request) {
@@ -39,4 +75,49 @@ func addItem(writer http.ResponseWriter, req *http.Request) {
 
 	writer.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(writer).Encode(items)
+}
+
+func updateItem(writer http.ResponseWriter, req *http.Request) {
+	var idParam string = mux.Vars(req)["id"]
+	id, err := strconv.Atoi(idParam)
+
+	if err != nil {
+		writer.WriteHeader(400)
+		writer.Write([]byte("ID couldn't be converted into an integer"))
+		return
+	}
+
+	if id >= len(items) {
+		writer.WriteHeader(404)
+		writer.Write([]byte("No item found with specified ID"))
+		return
+	}
+
+	var updatedItem Item
+	json.NewDecoder(req.Body).Decode(&updatedItem)
+
+	items[id] = updatedItem
+	writer.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(writer).Encode(updatedItem)
+}
+
+func deleteItem(writer http.ResponseWriter, req *http.Request) {
+	var idParam string = mux.Vars(req)["id"]
+	id, err := strconv.Atoi(idParam)
+
+	if err != nil {
+		writer.WriteHeader(400)
+		writer.Write([]byte("ID couldn't be converted into an integer"))
+		return
+	}
+
+	if id >= len(items) {
+		writer.WriteHeader(404)
+		writer.Write([]byte("No item found with specified ID"))
+		return
+	}
+
+	items = append(items[:id], items[id+1:]...)
+
+	writer.WriteHeader(200)
 }
